d30b5d7a4309af2bf8d83e4bfd0296dbc197db85
Aysha Aziz <aishaazizz30@gmail.com>
2026-02-21 21:47:58 +0400

    deploy issue

--- src/components/layout/navbar1.tsx
+++ src/components/layout/navbar1.tsx
@@ -65,7 +65,6 @@ const Navbar1 = ({ className }: Navbar1Props) => {
     { title: "Home", url: "/" },
     { title: "Find a tutor", url: "/tutor" },
     { title: "How it works", url: "/how-it-works" },
-    { title: "About", url: "/about" },
   ];
 
   const auth = {
