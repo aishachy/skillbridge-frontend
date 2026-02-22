cac24367d1a522ffb5b6cee7a0c22be596f9bef6
Aysha Aziz <aishaazizz30@gmail.com>
2026-02-22 21:56:48 +0400

    Loading error

--- src/app/tutor/page.tsx
+++ src/app/tutor/page.tsx
@@ -3,7 +3,7 @@ import StatsCard from "@/components/stats-card10";
 export const dynamic = "force-dynamic";
 
 export default async function TutorPage() {
-        await new Promise((resolve) => setTimeout(resolve, 1000))
+       
 
     return (
         <div>
