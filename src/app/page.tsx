038d2ad3c1e62854660ee0a2cce96c0e91f92531
Aysha Aziz <aishaazizz30@gmail.com>
2026-02-22 18:23:50 +0400

    about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import { Footer2 } from "@/components/footer2";
 import { Hero7 } from "@/components/hero7";
 import FeaturedTutor from "@/components/featuredTutor";
+import About from "./about/about";
 
 export default function Home() {
   return (
     <div>
       <Hero7></Hero7>
+      <About></About>
       <FeaturedTutor></FeaturedTutor>
       <Footer2></Footer2>
     </div>
