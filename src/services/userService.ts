1577711a6dbc10eb2552d2e1b67d529232b58d4a
Aysha Aziz <aishaazizz30@gmail.com>
2026-02-28 21:32:51 +0400

    user route

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -5,58 +5,61 @@ import { env } from "@/env";
 const API_URL = env.NEXT_PUBLIC_API_URL;
 
 export const getAllUsers = async () => {
-  const token = localStorage.getItem("accessToken");
+    const token = localStorage.getItem("accessToken");
 
-  const res = await fetch(`${API_URL}/admin/users`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const res = await fetch(`${API_URL}/admin/users`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
 
-  if (res.status === 403) {
-    throw new Error("You are not authorized (Admin only)");
-  }
+    const data = await res.json();
+    console.log(data);
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch users");
-  }
+    if (res.status === 403) {
+        throw new Error("You are not authorized (Admin only)");
+    }
 
-  return res.json();
+    if (!res.ok) {
+        throw new Error("Failed to fetch users");
+    }
+
+    return res.json();
 };
 
 export const deleteUser = async (id: number) => {
-  const token = localStorage.getItem("accessToken");
+    const token = localStorage.getItem("accessToken");
 
-  const res = await fetch(`${API_URL}/admin/users/${id}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const res = await fetch(`${API_URL}/admin/users/${id}`, {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
 
-  if (!res.ok) {
-    throw new Error("Delete failed");
-  }
+    if (!res.ok) {
+        throw new Error("Delete failed");
+    }
 
-  return res.json();
+    return res.json();
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const updateUser = async (id: number, data: any) => {
-  const token = localStorage.getItem("accessToken");
+    const token = localStorage.getItem("accessToken");
 
-  const res = await fetch(`${API_URL}/admin/users/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(data),
-  });
+    const res = await fetch(`${API_URL}/admin/users/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+    });
 
-  if (!res.ok) {
-    throw new Error("Update failed");
-  }
+    if (!res.ok) {
+        throw new Error("Update failed");
+    }
 
-  return res.json();
+    return res.json();
 };
